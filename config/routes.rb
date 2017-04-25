fe1951737f44fe323694c6ff5b16c759677d0990
Lena Zheng <lzhengem@gmail.com>
2017-04-25 01:33:27 +0000

    can pass letter to update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'stocks#index'
-  get 'stocks/test' => 'stocks#test'
+  resources :stocks
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
