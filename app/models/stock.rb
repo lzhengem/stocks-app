c2a0e69d2446a38a58cc0eb2974d416837b30502
Lena Zheng <lzhengem@gmail.com>
2017-11-02 06:35:37 +0000

    added get_latest_month_dividends

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -149,6 +149,28 @@ class Stock < ActiveRecord::Base
         dividends.to_f
     end    
     
+    def get_latest_month_dividends
+        dividends=0
+        rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+        if rev_doc.css('iframe#frmMain').any?
+            rev_chart_url = rev_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
+            rev_chart_doc = get_doc_from(rev_chart_url)
+            rev_chart = rev_chart_doc.css("td.body1")
+            rev_headers = rev_chart_doc.css("td.body1 b")
+        
+            if rev_headers[-2] #if the chart is avaiable for this symbol get rev info
+                latest_month = rev_headers[-2].text
+                start = 0
+                rev_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+                rev_info = rev_chart[start..-1] #contains all the relavent information rev_info 0-1 contains headers then 2-4 contains the info revenue from the last 3 years
+                
+                puts rev_info
+                #get dividends
+                dividends = rev_info[10].text
+            end
+        end
+        dividends.to_f
+    end
     # def get_dividends
     #     rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
     #     if rev_doc.css('iframe#frmMain').any?
