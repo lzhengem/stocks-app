061e90b124e91c521343e6ee11d3f4b84986b471
Lena Zheng <lzhengem@gmail.com>
2017-11-02 06:16:33 +0000

    added raise_exception_if_not_0_to_2, get_total_rev, get_latest_month_rev, get_total_eps, get_latest_month_eps

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,19 @@ class Stock < ActiveRecord::Base
         {price: ticker_doc.css("div#qwidget_lastsale.qwidget-dollar").text.tr('$','').to_f}
     end
     
-    def get_rev
+    def raise_exception_if_not_0_to_2(years_ago)
+        unless [0,1,2].include? years_ago
+            raise "Expected 0, 1, or 2 years ago"
+        end
+    end
+
+    def get_total_rev(years_ago = 0)
+        #if it gives a number other than 0,1,2 there is no data for that set
+        raise_exception_if_not_0_to_2(years_ago)
+        
+        #get the revenue-eps page
         rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+        return_rev = 0
         if rev_doc.css('iframe#frmMain').any?
                 rev_chart_url = rev_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
                 rev_chart_doc = get_doc_from(rev_chart_url)
@@ -22,36 +33,48 @@ class Stock < ActiveRecord::Base
                     #rev_info 5 is latest month eps header, 6-8 contains eps info for last 3 years
                     #rev_info 13 contains total headers, rev_info 14 contains total rev header
                     #if there is info in the latest month, then we can use totals to calculate rev and eps
-                    rev_curr_year = rev_info[15].text.scan(/[\w+]/).join.get_full_number
-                    rev_last_year = rev_info[16].text.scan(/[\w+]/).join.get_full_number
-                    rev_last_2_year = rev_info[17].text.scan(/[\w+]/).join.get_full_number
                     
-                    if rev_curr_year.zero? #if info for the latest month is missing, instead of using totals, use previous month
-                        latest_month = rev_headers[-3].text
-                        rev_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
-                        rev_info = rev_chart[start..-1]
-                        rev_curr_year = rev_info[2].text.scan(/[\w+]/).join.get_full_number
-                        rev_last_year = rev_info[3].text.scan(/[\w+]/).join.get_full_number
-                        rev_last_2_year = rev_info[4].text.scan(/[\w+]/).join.get_full_number
-                    end
-                else
-                    rev_curr_year = rev_last_year = rev_last_2_year = 0
+                    # rev_curr_year = rev_info[15].text.scan(/[\w+]/).join.get_full_number
+                    # rev_last_year = rev_info[16].text.scan(/[\w+]/).join.get_full_number
+                    # rev_last_2_year = rev_info[17].text.scan(/[\w+]/).join.get_full_number
+                    #current year rev is at 15, last year's rev is at 16, 2 year's ago rev is at 17
+                    return_rev = rev_info[15 + years_ago].text.scan(/[\w+]/).join.get_full_number
                 end
-        else
-            rev_curr_year = rev_last_year = rev_last_2_year = 0
         end
-        
-        if rev_curr_year ==  0
-            rev_score = "N/A"
-        elsif rev_curr_year > rev_last_year and rev_last_year > rev_last_2_year
-            rev_score = "Pass"
-        else 
-            rev_score = "Fail"
-        end
-        {rev_curr_year: rev_curr_year, rev_last_year: rev_curr_year, rev_last_2_year: rev_last_2_year, rev_score: rev_score}
+        return_rev
     end
     
-    def get_eps
+    def get_latest_month_rev(years_ago = 0)
+        #if it gives a number other than 0,1,2 there is no data for that set
+        raise_exception_if_not_0_to_2(years_ago)
+        
+        rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+        if rev_doc.css('iframe#frmMain').any?
+            start = 0
+            return_rev = 0
+            rev_chart_url = rev_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
+            rev_chart_doc = get_doc_from(rev_chart_url)
+            rev_chart = rev_chart_doc.css("td.body1")
+            rev_headers = rev_chart_doc.css("td.body1 b")
+            
+            #check which is the latest month with data
+            latest_month = rev_headers[-3].text
+            rev_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+            rev_info = rev_chart[start..-1]
+            # rev_curr_year = rev_info[2].text.scan(/[\w+]/).join.get_full_number
+            # rev_last_year = rev_info[3].text.scan(/[\w+]/).join.get_full_number
+            # rev_last_2_year = rev_info[4].text.scan(/[\w+]/).join.get_full_number
+            #latest month rev info is at 2, 1 year ago at index 3, 2 years ago at index 4
+            return_rev = rev_info[2 + years_ago].text.scan(/[\w+]/).join.get_full_number
+        end
+        return_rev
+    end
+
+    def get_total_eps(years_ago = 0)
+        #if it gives a number other than 0,1,2 there is no data for that set
+        raise_exception_if_not_0_to_2(years_ago)
+        
+        return_eps =0
         #get eps information
         eps_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
         if eps_doc.css('iframe#frmMain').any?
@@ -66,38 +89,135 @@ class Stock < ActiveRecord::Base
                 eps_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)} #looping through the December (FYE) info
                 eps_info = eps_chart[start..-1]
                 #get eps info
-                eps_curr_year = eps_info[19].text.to_f
-                eps_last_year = eps_info[20].text.to_f
-                eps_last_2_year = eps_info[21].text.to_f
-                
-                if eps_curr_year.zero? #if info for the latest month is missing, instead of using totals, use pepsious month
-                    latest_month = eps_headers[-3].text
-                    eps_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
-
-                    #get eps info    
-                    eps_curr_year = eps_info[6].text.to_f
-                    eps_last_year = eps_info[7].text.to_f
-                    eps_last_2_year = eps_info[8].text.to_f
-                    
-                end
-            else
-                eps_curr_year = eps_last_year = eps_last_2_year = 0
-
+                # eps_curr_year = eps_info[19].text.to_f
+                # eps_last_year = eps_info[20].text.to_f
+                # eps_last_2_year = eps_info[21].text.to_f
+                #eps this year is at index 19, last year at 20, 2 years ago at 21
+                return_eps = eps_info[19 + years_ago].text.to_f
             end
-        else
-            eps_curr_year = eps_last_year = eps_last_2_year = 0
         end
-            
-        if eps_curr_year ==  0
-            eps_score = "N/A"
-        elsif eps_curr_year > eps_last_year and eps_last_year > eps_last_2_year
-            eps_score = "Pass"
-        else 
-            eps_score = "Fail"
-        end
-        {eps_curr_year: eps_curr_year, eps_last_year: eps_last_year, eps_last_2_year: eps_last_2_year, eps_score: eps_score}
-        
+        return_eps
     end
+    
+    def get_latest_month_eps(years_ago = 0)
+        #if it gives a number other than 0,1,2 there is no data for that set
+        raise_exception_if_not_0_to_2(years_ago)
+        
+        #get eps information
+        eps_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+        return_eps = 0
+        if eps_doc.css('iframe#frmMain').any?
+            eps_chart_url = eps_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
+            eps_chart_doc = get_doc_from(eps_chart_url)
+            eps_chart = eps_chart_doc.css("td.body1")
+            eps_headers = eps_chart_doc.css("td.body1 b") 
+            start = 0
+            
+            
+            latest_month = eps_headers[-3].text
+            eps_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+            eps_info = eps_chart[start..-1]
+            #get eps info    
+            # eps_curr_year = eps_info[6].text.to_f
+            # eps_last_year = eps_info[7].text.to_f
+            # eps_last_2_year = eps_info[8].text.to_f
+            #eps latest month this year is at index 6, last year at 7, 2 years ago at 8
+            return_eps = eps_info[6 + years_ago].text.to_f
+        end
+        return_eps
+    end
+    # def get_rev
+    #     rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+    #     if rev_doc.css('iframe#frmMain').any?
+    #             rev_chart_url = rev_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
+    #             rev_chart_doc = get_doc_from(rev_chart_url)
+    #             rev_chart = rev_chart_doc.css("td.body1")
+    #             rev_headers = rev_chart_doc.css("td.body1 b")
+            
+    #             if rev_headers[-2] #if the chart is avaiable for this symbol get rev info
+    #                 latest_month = rev_headers[-2].text
+    #                 start = 0
+    #                 rev_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+    #                 rev_info = rev_chart[start..-1] #contains all the relavent information rev_info 0-1 contains headers then 2-4 contains the info revenue from the last 3 years
+    #                 #rev_info 5 is latest month eps header, 6-8 contains eps info for last 3 years
+    #                 #rev_info 13 contains total headers, rev_info 14 contains total rev header
+    #                 #if there is info in the latest month, then we can use totals to calculate rev and eps
+    #                 rev_curr_year = rev_info[15].text.scan(/[\w+]/).join.get_full_number
+    #                 rev_last_year = rev_info[16].text.scan(/[\w+]/).join.get_full_number
+    #                 rev_last_2_year = rev_info[17].text.scan(/[\w+]/).join.get_full_number
+                    
+    #                 if rev_curr_year.zero? #if info for the latest month is missing, instead of using totals, use previous month
+    #                     latest_month = rev_headers[-3].text
+    #                     rev_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+    #                     rev_info = rev_chart[start..-1]
+    #                     rev_curr_year = rev_info[2].text.scan(/[\w+]/).join.get_full_number
+    #                     rev_last_year = rev_info[3].text.scan(/[\w+]/).join.get_full_number
+    #                     rev_last_2_year = rev_info[4].text.scan(/[\w+]/).join.get_full_number
+    #                 end
+    #             else
+    #                 rev_curr_year = rev_last_year = rev_last_2_year = 0
+    #             end
+    #     else
+    #         rev_curr_year = rev_last_year = rev_last_2_year = 0
+    #     end
+        
+    #     if rev_curr_year ==  0
+    #         rev_score = "N/A"
+    #     elsif rev_curr_year > rev_last_year and rev_last_year > rev_last_2_year
+    #         rev_score = "Pass"
+    #     else 
+    #         rev_score = "Fail"
+    #     end
+    #     {rev_curr_year: rev_curr_year, rev_last_year: rev_curr_year, rev_last_2_year: rev_last_2_year, rev_score: rev_score}
+    # end
+    
+    # def get_eps
+    #     #get eps information
+    #     eps_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
+    #     if eps_doc.css('iframe#frmMain').any?
+    #         eps_chart_url = eps_doc.css('iframe#frmMain').first['src'] #gets the page where the chart resides
+    #         eps_chart_doc = get_doc_from(eps_chart_url)
+    #         eps_chart = eps_chart_doc.css("td.body1")
+    #         eps_headers = eps_chart_doc.css("td.body1 b") 
+        
+    #         if eps_headers[-2] ##looking at the last quarter: December(FYE) if the chart is avaiable for this symbol get eps info
+    #             latest_month = eps_headers[-2].text
+    #             start = 0
+    #             eps_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)} #looping through the December (FYE) info
+    #             eps_info = eps_chart[start..-1]
+    #             #get eps info
+    #             eps_curr_year = eps_info[19].text.to_f
+    #             eps_last_year = eps_info[20].text.to_f
+    #             eps_last_2_year = eps_info[21].text.to_f
+                
+    #             if eps_curr_year.zero? #if info for the latest month is missing, instead of using totals, use pepsious month
+    #                 latest_month = eps_headers[-3].text
+    #                 eps_chart.each_with_index{|node, index| start = index if node.text.include?(latest_month)}
+
+    #                 #get eps info    
+    #                 eps_curr_year = eps_info[6].text.to_f
+    #                 eps_last_year = eps_info[7].text.to_f
+    #                 eps_last_2_year = eps_info[8].text.to_f
+                    
+    #             end
+    #         else
+    #             eps_curr_year = eps_last_year = eps_last_2_year = 0
+
+    #         end
+    #     else
+    #         eps_curr_year = eps_last_year = eps_last_2_year = 0
+    #     end
+            
+    #     if eps_curr_year ==  0
+    #         eps_score = "N/A"
+    #     elsif eps_curr_year > eps_last_year and eps_last_year > eps_last_2_year
+    #         eps_score = "Pass"
+    #     else 
+    #         eps_score = "Fail"
+    #     end
+    #     {eps_curr_year: eps_curr_year, eps_last_year: eps_last_year, eps_last_2_year: eps_last_2_year, eps_score: eps_score}
+        
+    # end
     
     def get_dividends
         rev_doc = get_doc_from("http://www.nasdaq.com/symbol/#{name}/revenue-eps")
